16d55a71cca4ec0f34beddc445dc5a55eaa0327c
Diego  Jesus <diego.a.r.jz@gmail.com>
2019-07-11 21:31:01 +0100

    Multiple issues addressed.
    Reimplements the graph reader to use an abstract syntax tree. Closes #33
    Adds a ParameterNode. Closes #34
    Implements parameters in graphs through the ParameterNode. Closes #19
    Allows graph files to specify parameters. Closes #26
    Expressions in parameters can use graph parameters. Closes #25

--- tests/regression_tests/regression_test.py
+++ tests/regression_tests/regression_test.py
@@ -43,3 +43,16 @@ def expressions_in_parameters(test):
 
     test.tester.file_contents_should_match("geom0.obj")
     test.tester.file_contents_should_match("expression.dot")
+
+@deregress.test
+def parameter_overwrite(test):
+    test.runner.executable(PATH_TO_GRAPH_RUNNER) \
+        .arg("--input-file={}".format(os.path.join(THIS_DIRECTORY, "parameter_overwrite.sel"))) \
+        .arg("--execute") \
+        .arg("--export-geometry") \
+        .arg("--export-path=.") \
+        .arg("--dot=parameter_overwrite.dot") \
+        .run()
+
+    test.tester.file_contents_should_match("geom0.obj")
+    test.tester.file_contents_should_match("parameter_overwrite.dot")
