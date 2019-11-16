a4ab42f3a1e787bfb65c41ba0d2e0a0c5dc58cb4
Diego  Jesus <diego.a.r.jz@gmail.com>
2019-11-16 19:09:18 +0000

    Implements the extract faces operation.
    Closes #8

---
+++ source/geometry_operations/explode_to_faces.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "common/assertions.h"
+#include "common/logger.h"
+#include "common/profiler.h"
+#include "geometry_core/geometry.h"
+#include "geometry_core/geometry_builder.h"
+
+namespace selector
+{
+template<class G>
+class ExplodeToFaces
+{
+private:
+	using Geometry = G;
+	using GeometryPtr = std::shared_ptr<Geometry>;
+
+public:
+	ExplodeToFaces() {}
+
+	template<class Container>
+	void Execute(GeometryPtr geometryIn, Container &outGeometries)
+	{
+		START_PROFILE;
+		LOG_TRACE(GeometryOperations, "Exploding to faces");
+
+		outGeometries.reserve(geometryIn->GetFaceCount());
+
+		for (auto fIter = geometryIn->FacesBegin(); fIter != geometryIn->FacesEnd(); ++fIter)
+		{
+			auto faceGeometry = std::make_shared<Geometry>();
+			GeometryBuilderT<Geometry> builder(faceGeometry);
+
+			auto faceBuilder = builder.StartFace();
+			for (auto fpCirc = geometryIn->FacePointCirculatorBegin(*fIter); fpCirc; ++fpCirc)
+			{
+				faceBuilder.AddIndex(builder.AddPoint(geometryIn->GetPosition(*fpCirc)));
+			}
+			faceBuilder.CloseFace();
+
+			outGeometries.push_back(faceGeometry);
+		}
+	}
+};
+}  // namespace selector
