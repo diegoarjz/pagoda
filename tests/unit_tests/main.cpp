2dd867268012e37381f43369ab80d1b260520469
Diego  Jesus <diego.a.r.jz@gmail.com>
2020-02-28 11:02:20 +0000

    Fixes Unit Tests

--- tests/unit_tests/main.cpp
+++ tests/unit_tests/main.cpp
@@ -6,13 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    SelectorTestFixtureBase::SetExecutablePath(argv[0]);
-    SelectorTestFixtureBase::SetShouldWriteFiles(false);
-    ::testing::InitGoogleTest(&argc, argv);
+	SelectorTestFixtureBase::SetExecutablePath(argv[0]);
+	SelectorTestFixtureBase::SetShouldWriteFiles(false);
+	::testing::InitGoogleTest(&argc, argv);
 
-    auto returnVal = RUN_ALL_TESTS();
+	auto returnVal = RUN_ALL_TESTS();
 
-    selector::Logger::Shutdown();
+	selector::Logger::Shutdown();
 
-    return returnVal;
+	return returnVal;
 }
