89e6d4f7af3ab7f038c7d51b5acde1416ade4fa8
Diego  Jesus <diego.a.r.jz@gmail.com>
2020-10-26 19:17:07 +0000

    Fix yaml

--- conanfile.py
+++ conanfile.py
@@ -31,9 +31,6 @@ class Pagoda(ConanFile):
 
 
     def build(self):
-        if self.settings.os == "Windows":
-            cmake = CMake(self, generator="MinGW Makefiles", build_type=self.settings.build_type)
-        else:
-            cmake = CMake(self, build_type=self.settings.build_type)
+        cmake = CMake(self, build_type=self.settings.build_type)
         cmake.configure()
         cmake.build()
