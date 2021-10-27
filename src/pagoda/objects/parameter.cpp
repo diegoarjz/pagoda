3071559945f2af3891663c3abf872b64c9000ca7
Diego  Jesus <diego.a.r.jz@gmail.com>
2021-10-27 08:25:28 +0100

    Implement expressions in parameters

--- src/pagoda/objects/parameter.cpp
+++ src/pagoda/objects/parameter.cpp
@@ -1,7 +1,16 @@
 #include "parameter.h"
 
+#include <pagoda/pgscript/interpreter/interpreter.h>
+#include <pagoda/pgscript/parser/parser.h>
+
 namespace pagoda::objects
 {
+struct ParameterBase::Expression
+{
+	std::string m_expression;
+	ast::ProgramPtr m_compiledExpression;
+};
+
 ParameterBase::ParameterBase(const std::string& name)
   : m_name{name}, m_label{name}
 {
@@ -39,7 +48,54 @@ const std::string& ParameterBase::GetLabel() const
 }
 
 ////////////////////////////////////////
-// \name Conversion and Serialization
+/// \name Expressions
+////////////////////////////////////////
+bool ParameterBase::HasExpression() const
+{
+	return m_expression != nullptr;
+}
+
+void ParameterBase::SetExpression(const std::string& e)
+{
+	if (m_expression == nullptr) {
+		m_expression = std::make_unique<Expression>();
+	}
+	m_expression->m_expression = e;
+}
+
+const std::string& ParameterBase::GetExpression() const
+{
+	static const std::string& sEmptyExpression{""};
+	if (m_expression == nullptr) {
+		return sEmptyExpression;
+	}
+	return m_expression->m_expression;
+}
+
+void ParameterBase::EvaluateExpression()
+{
+	if (m_expression == nullptr) {
+		return;
+	}
+
+	if (m_expression->m_compiledExpression == nullptr) {
+		m_expression->m_compiledExpression =
+		  Parser().Parse(m_expression->m_expression);
+	}
+
+	if (m_expression->m_compiledExpression == nullptr) {
+		return;
+	}
+
+	Interpreter interpreter;
+	interpreter.Interpret(m_expression->m_compiledExpression);
+	if (auto value = interpreter.GetLastEvaluatedExpression()) {
+		FromDynamicValue(value);
+	}
+}
+
+////////////////////////////////////////
+/// \name Conversion and Serialization
 ////////////////////////////////////////
 
 template<>
