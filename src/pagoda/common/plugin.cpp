548d8efc8efb8e342ba2d3106459feab9b7df1f0
Diego  Jesus <diego.a.r.jz@gmail.com>
2023-04-09 14:43:26 +0100

    Allow setting the plugin loading dir from a env var

--- src/pagoda/common/plugin.cpp
+++ src/pagoda/common/plugin.cpp
@@ -4,16 +4,20 @@
 
 #include "pagoda/common/fs/file_util.h"
 
-namespace pagoda::common
-{
-  const std::string& Plugin::GetPluginPath()
-  {
-    using namespace boost::filesystem;
-    static std::string pluginDir;
-    if (pluginDir.empty()) {
+namespace pagoda::common {
+const std::string &Plugin::GetPluginPath() {
+  static const auto pluginDirEnv = std::getenv("PAGODA_PLUGIN_PATH");
+  using namespace boost::filesystem;
+  static std::string pluginDir;
+  if (pluginDir.empty()) {
+    if (pluginDirEnv != nullptr &&
+        std::filesystem::is_directory(pluginDirEnv)) {
+      pluginDir = pluginDirEnv;
+    } else {
       path executablePath = common::fs::GetExecutablePath();
       pluginDir = (executablePath.parent_path() / path("plugins")).string();
     }
-    return pluginDir;
   }
+  return pluginDir;
 }
+} // namespace pagoda::common
