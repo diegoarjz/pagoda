bb93ac850d8a9c5c9d85535d647a768500486641
Diego Jesus <diego.a.r.jz@gmail.com>
2025-03-30 17:03:42 +0100

    Reimplement forward graph traversal to account for multiple inputs

--- src/pagoda/graph/traversal/forward.cpp
+++ src/pagoda/graph/traversal/forward.cpp
@@ -1,5 +1,8 @@
 #include "forward.h"
+#include "pagoda/graph/node.h"
 
+#include <algorithm>
+#include <map>
 #include <pagoda/graph/query/input_node.h>
 #include <iterator>
 
@@ -9,8 +12,51 @@ namespace pagoda::graph::traversal
 {
 Forward::Forward(Graph& graph) : Traversal(graph)
 {
-	query::InputNode q(graph, [this](NodePtr n) { m_nodesToVisit.push(n); });
+  std::queue<NodePtr> nodes;
+  std::map<NodePtr, uint32_t> nodeDistances;
+	query::InputNode q(graph, [&nodes, &nodeDistances](NodePtr n) {
+    nodes.push(n);
+    nodeDistances.emplace(n, 0);
+  });
 	graph.ExecuteQuery(q);
+
+  while (!nodes.empty()) {
+    NodePtr front = nodes.front();
+    nodes.pop();
+    const uint32_t thisNodeDistance = nodeDistances[front];
+
+    NodeSet outNodes;
+    GetOutputNodes(front, std::inserter(outNodes, std::end(outNodes)));
+    for (auto n : outNodes) {
+      auto iter = nodeDistances.find(n);
+      const auto nextNodeDistance = iter == nodeDistances.end() ?
+                                      0 :             // First time seeing this node
+                                      iter->second;   // We've seen this node
+
+      // Update distance
+      if (thisNodeDistance + 1 > nextNodeDistance) {
+        // We have found a longer path
+        nodeDistances[n] = iter->second = thisNodeDistance + 1;
+      }
+
+      nodes.push(n);
+    }
+  }
+
+  std::vector<NodePtr> sortedNodes;
+  sortedNodes.reserve(nodeDistances.size());
+  for (const auto& [n, dist] : nodeDistances) {
+    sortedNodes.emplace_back(n);
+  }
+
+  std::sort(sortedNodes.begin(), sortedNodes.end(),
+    [&nodeDistances] (const NodePtr& lhs, const NodePtr& rhs) {
+      return nodeDistances[lhs] < nodeDistances[rhs];
+  });
+
+  for (const auto& n : sortedNodes) {
+    m_nodesToVisit.push(n);
+  }
 }
 
 Forward::~Forward() {}
@@ -19,13 +65,7 @@ NodePtr Forward::Get() { return m_nodesToVisit.front(); }
 
 bool Forward::Advance()
 {
-	auto front = m_nodesToVisit.front();
-	m_nodesToVisit.pop();
-	NodeSet outNodes;
-	GetOutputNodes(front, std::inserter(outNodes, std::end(outNodes)));
-	for (auto n : outNodes) {
-		m_nodesToVisit.push(n);
-	}
+  m_nodesToVisit.pop();
 	return HasNext();
 }
 
